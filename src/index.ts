0f0e0169e506df258d9b08042c84d2c0a4fd7586
Alex Barbero <barrufat@hotmail.com>
2023-10-20 23:22:21 +0200

    Add getPokemon function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-const num = 3;
+import { type Pokemon, type PokemonResponse } from "./types";
 
-console.log(num);
+const urlPrefix = "https://pokeapi.co/api/v2/pokemon?offset=";
+const urlSufix = "&limit=10";
+
+const page = "0";
+
+const getPokemons = async (
+  prefix: string,
+  sufix: string,
+  page: string,
+): Promise<Pokemon[]> => {
+  const response = await fetch(prefix + page + sufix);
+  const pokemons = (await response.json()) as PokemonResponse;
+
+  return pokemons.results.map((pokemon: Pokemon) => pokemon);
+};
+
+const pokemons = await getPokemons(urlPrefix, page, urlSufix);
+
+pokemons.forEach((pokemon: Pokemon) => {
+  console.log(pokemon);
+});
