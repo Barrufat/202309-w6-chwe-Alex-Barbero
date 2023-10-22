7717246923f89a657102704142ee0d1c8e7e5fb2
Alex Barbero <barrufat@hotmail.com>
2023-10-22 12:47:06 +0200

    Add PokemonCard component

---
+++ src/components/PokemonCard/PokemonCard.ts
@@ -0,0 +1,24 @@
+import type { PokemonDetails } from "../../types";
+import Component from "../Component/Component.js";
+
+class PokemonCard extends Component {
+  constructor(
+    parentElement: Element,
+    private readonly pokemonDetails: PokemonDetails,
+  ) {
+    super(parentElement, "div", "pokemon-card");
+  }
+
+  protected populate(): void {
+    this.element.innerHTML = `
+    <article class="pokemon-card__box">
+    <h1 class="pokemon-card__title">${this.pokemonDetails.name}</h1>
+    <img class="pokemon-card__img src="${this.pokemonDetails.sprites.other["official-artwork"].front_shiny}"
+     alt="${this.pokemonDetails.name}" width="400" height"400"/>
+    <span class="pokemon-card__detail>Number: #${this.pokemonDetails.id}</span>
+    </article>
+    `;
+  }
+}
+
+export default PokemonCard;
