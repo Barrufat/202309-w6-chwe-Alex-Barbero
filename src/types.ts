577a29f327f6f8a1566f48b98eaa78d86498fe93
Alex Barbero <barrufat@hotmail.com>
2023-10-22 12:23:05 +0200

    Add index.html

--- src/types.ts
+++ src/types.ts
@@ -10,24 +10,48 @@ export interface Pokemon {
   url: string;
 }
 
-// Export interface Pokemon {
-//   abilities: [];
-//   base_experience: number;
-//   forms: [];
-//   game_indices: [];
-//   height: 20;
-//   held_items: [];
-//   id: 3;
-//   is_default: boolean;
-//   location_area_encounters: string;
-//   moves: [];
-//   name: string;
-//   order: number;
-//   past_abilities: [];
-//   past_types: [];
-//   species: Record<string, unknown>;
-//   sprites: Record<string, unknown>;
-//   stats: [];
-//   types: [];
-//   weight: 1000;
-// }
+export interface PokemonDetails {
+  abilities: unknown[];
+  base_experience: number;
+  forms: unknown[];
+  game_indices: unknown[];
+  height: number;
+  held_items: unknown[];
+  id: number;
+  is_default: boolean;
+  location_area_encounters: string;
+  moves: unknown[];
+  name: string;
+  order: number;
+  past_abilities: unknown[];
+  past_types: unknown[];
+  species: Record<string, unknown>;
+  sprites: PokeSprites;
+  stats: unknown[];
+  types: unknown[];
+  weight: number;
+}
+
+export interface PokeSprites {
+  back_default: string;
+  back_female: string;
+  back_shiny: string;
+  back_shiny_female: undefined;
+  front_default: string;
+  front_female: string;
+  front_shiny: string;
+  front_shiny_female: string;
+  other: Other;
+  versions: Record<string, unknown>;
+}
+
+export interface Other {
+  dream_world: Record<string, unknown>;
+  home: Record<string, unknown>;
+  "official-artwork": OfficialArtWork;
+}
+
+export interface OfficialArtWork {
+  front_default: string;
+  front_shiny: string;
+}
